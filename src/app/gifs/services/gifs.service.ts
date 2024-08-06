85d4d605c02328b891129440e29aff072e7afd2f
Jose <jaibanez795@gmail.com>
2024-08-06 00:49:37 -0600

    Service: Updated with Filter & Max10

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,8 +10,19 @@ export class GifsService {
     return [...this._tagHistory];
   }
 
-  public searchTag(tag:string):void{
+  private orginizeHistory(tag:string){
+    tag = tag.toLowerCase();
+    if(this._tagHistory.includes(tag)){
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag)
+    }
     this._tagHistory.unshift(tag);
+    this._tagHistory = this._tagHistory.splice(0,10);
+  }
+
+  public searchTag(tag:string):void{
+    if(tag.length === 0)return;
+    this.orginizeHistory(tag);
+    // this._tagHistory.unshift(tag);
     console.log(this.tagsHistory);
 
   }
